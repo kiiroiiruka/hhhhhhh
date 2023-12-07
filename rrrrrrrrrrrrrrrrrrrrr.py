5eda6bf6a5fbb6eace92b58990252162fc96f48c
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2023-12-07 09:49:52 +0900

    Create rrrrrrrrrrrrrrrrrrrrr.py

---
+++ rrrrrrrrrrrrrrrrrrrrr.py
@@ -0,0 +1,28 @@
+import pgzrun
+moji="abcdefg"
+lis=[]
+def draw():
+
+    screen.draw.text(moji,(500,300))
+
+print("ばあああああああああああああああああああああああああああああああああか")
+class nam:
+    def __init__(self) -> None:
+        pass
+    def damn(self,aa)-> None:
+        return "kdfdsgdsg"
+class man(nam):
+    pass
+    def __str__(self):
+        return "これはあほ"
+b=man()
+b.damn("asauduyf")
+a=nam()
+print(a)
+for _ in range(0,100):
+    print("aa"+str(_))
+n=0
+while n<100:
+    n+=1
+
+pgzrun.go()
